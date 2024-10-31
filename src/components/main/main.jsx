74a608bbbe16bec1335889257f58e4739752d50c
lowkieman <lowkieman@gmail.com>
2024-10-31 11:10:00 +0300

    updated the sidebar color

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -9,30 +9,14 @@ const Main = () => {
                 <p><span>AiCE</span></p>
                 <img src={assets.aice} alt="" />
             </div>
+
             <div className="main-container">
                 <p className="greet">
                     <p><span>Hello, Dev.</span></p>
                     <p>how can I help you today?</p>
                 </p>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.explore} alt="" />
-                </div>
-                <div className="card">
-                    <p>Briefly summarize this concept: urban planning</p>
-                    <img src={assets.lightbulb} alt="" />
-                </div>
-                <div className="card">
-                    <p>Brainstorm team bonding activities for our work retreat</p>
-                    <img src={assets.chat} alt="" />
-                </div> 
-                <div className="card">
-                    <p>Improve the readability of the following code</p>
-                    <img src={assets.code} alt="" />
-                </div>
-            </div>
+
             <div className="main-bottom">
                 <div className="search-box">
                     <input type="text" placeholder="Enter a prompt here" />
