630b6318c77f8b35c679b466eba18bdac9a7f848
lowkieman <lowkieman@gmail.com>
2024-11-19 21:05:00 +0300

    config files

---
+++ src/config/Gemini.js
@@ -0,0 +1,31 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+configs()
+  
+  const apiKey = process.env.GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-pro",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runChat(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const prompt = "What is react js"
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+  }
+  
+  runChat();
